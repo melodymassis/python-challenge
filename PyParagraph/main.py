ea539e99fa50f0e64a02982af1b02171e7e0e279
MelodyLamphear <melodymassis@gmail.com>
2018-02-04 22:43:10 -0800

    Word Count works

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -1,1 +1,17 @@
 #PyParagraph
+
+
+import re
+
+#filename = "words.txt"
+
+words = "the quick brown fox jumps. Over the fence".split()
+#print(words)
+
+#idx_to_words = {i:w for i,w in enumerate(words)}
+#print(idx_to_words)
+
+num_words = len(words)
+print("Approximate word count ")
+print(num_words)
+
