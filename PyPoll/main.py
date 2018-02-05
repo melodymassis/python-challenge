f4b1e31ea9a4b7192e27f24321afa5e1e2d366b0
MelodyLamphear <melodymassis@gmail.com>
2018-02-04 16:29:35 -0800

    Working code to write new csv

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -6,13 +6,15 @@ from collections import defaultdict
 from collections import Counter
 
 #Determine which csv file version to grab
-FileVersion = ['1','2']
+FileVersion = ['3']
 
 #For loop
 for FileToCheck in FileVersion: 
     #input file name
     csvpath = os.path.join('election_data_' + FileToCheck + '.csv' )
 
+    #create new CVS
+    PollResults = os.path.join('Poll_Results' + FileToCheck + '.csv')
 #lists to store data
     Voter_ID = []
     County = []
@@ -23,8 +25,8 @@ for FileToCheck in FileVersion:
     Winner = []
 
     
-    with open (csvpath, 'r') as csvpath:
-        csvreader = csv.reader(csvpath, delimiter=",")
+    with open (csvpath, 'r') as csvfile:
+        csvreader = csv.reader(csvfile, delimiter=",")
         # Skipp headers
         next(csvreader, None)
         
@@ -32,16 +34,18 @@ for FileToCheck in FileVersion:
             
             #append data for candidate
             Candidate.append(row[2])
-            Total_Votes = defaultdict(Candidate)
 
-            Total_by_Cand = {}
-            for w in range(len(Candidate)):
-                if w in Total_by_Cand:
-                    Total_by_Cand[w]+=1
-                else:
-                    Total_by_Cand[w]=1
-            print (Total_by_Cand) 
-            
+    cleanCSV = zip(Candidate)
+    with open(PollResults, 'w', newline="") as csvfile:
+
+        csvWriter = csv.writer(csvfile, delimiter=',')
+        #Add headers to new CSV
+        csvWriter.writerow(["Candidate"])
+
+        #Write zipped lists to csv
+        csvWriter.writerows(cleanCSV)
+
+           
 
 
             
