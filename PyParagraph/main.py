e4d23e2287a3e432f90f0fa6b3410a6a72430ec8
MelodyLamphear <melodymassis@gmail.com>
2018-02-04 23:37:46 -0800

    Code sans import text file

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -12,10 +12,21 @@ words = "the quick brown fox jumps. Over the fence".split()
 #print(idx_to_words)
 
 num_words = len(words)
-print("Approximate word count ")
-print(num_words)
+print("\n")
+print("Paragraph Analysis", "\n")
+print("-------------------","\n")
+print("Approximate word count: ",num_words)
 
 sentences = "the quick brown. fox jumps. Over the fence".split('.')
 num_sent = len(sentences)
-print("Approximate sentence count ")
-print(num_sent)
+print("Approximate sentence count: ",num_sent)
+
+
+num_let = len("the quick brown. fox jumps. Over the fence")
+print("Approximate letter count: ",num_let)
+
+avg_let = num_let/num_words
+print("Average letter count: ",avg_let)
+
+avg_sen = num_words/num_sent
+print("Average Sentence Lenght: ",avg_sen,"\n")
