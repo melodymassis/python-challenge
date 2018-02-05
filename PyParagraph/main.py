d6aa53da723997c17a7a6d849a1a18c6da36c5aa
MelodyLamphear <melodymassis@gmail.com>
2018-02-04 22:51:13 -0800

    Word and sentence count works

--- PyParagraph/main.py
+++ PyParagraph/main.py
@@ -15,3 +15,7 @@ num_words = len(words)
 print("Approximate word count ")
 print(num_words)
 
+sentences = "the quick brown. fox jumps. Over the fence".split('.')
+num_sent = len(sentences)
+print("Approximate sentence count ")
+print(num_sent)
