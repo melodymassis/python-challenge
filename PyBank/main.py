56a5f33e094c4e749215b6d6a605a2bf3abe38ea
MelodyLamphear <melodymassis@gmail.com>
2018-02-05 23:46:47 -0800

    PyBank Complete

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,8 +3,7 @@
 import os
 import csv
 from collections import defaultdict
-from collections import Counter
-
+import numpy as np
 
 #Determine which csv file version to grab
 #FileVersion = ['1']
@@ -25,19 +24,33 @@ with open(csvfile, 'r') as RawData, open(NewCSV, 'w') as f_out:
 
     Month_count = 0
     Total_Rev = 0
+    ListValues = []
+    Max_Inc = 0
+
+
     for record in d_reader:
         record['Date'] = str(record['Date'])
         Month_count+=1
         record['Revenue'] = int(record['Revenue'])
         d_writer.writerow(record)
         Total_Rev+=record['Revenue']
+        ListValues.append(int(record['Revenue']))
+    x = np.array(ListValues)
+    Monthly_Change = np.diff(x)
+    Avg_Change = sum(Monthly_Change)/Month_count
+    Max_Inc = max(Monthly_Change)
+    Min_Inc = min(Monthly_Change)
     
-    #print(record)
+
+    print("\n")
     print("Financial Analysis", "\n")
     print("-------------------","\n")
     print("Total Months: ", Month_count)
-    print("Total Revenue: ", Total_Rev)
-    
+    print("Total Revenue: $", Total_Rev)
+    print("Average Revenue Change: $", Avg_Change)
+    print("Greatest Increase in Revenue: $", Max_Inc)
+    print("Greatest Decrease in Revenue: $", Min_Inc)
+    print("\n")
 
 
 
